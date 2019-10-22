7c404819458def8090dc73c561cb10e9b753f62a
VoxoR7 <mvaudeleau@laposte.net>
2019-10-22 19:18:40 +0200

    fin du TP3

--- TP_Vers/Signaux/aire.c
+++ TP_Vers/Signaux/aire.c
@@ -21,9 +21,6 @@ char Nom_Prog[256] ;
 
 int fd, nbVers, continu;
 
-int verrou ( int fd, short l_type, short l_whence, long l_start, long l_len, short l_pid, int ope );
-int verrou9cases( int fd, int x, int y, off_t fdEmp, int poser );
-
 /*
  * HANDLER
  */
@@ -65,9 +62,6 @@ void handler ( int sig, siginfo_t *info, void* useless ) {
 
         tab[i] = info->si_pid;
 
-        if ( verrou( fd, F_WRLCK, 0, 0, 0, getpid(), F_SETLKW) )
-		    exit(-1);
-
         if ( terrain_dim_lire(fd, &nbLigne, &nbCol) )
             exit(-2);
 
@@ -79,22 +73,12 @@ void handler ( int sig, siginfo_t *info, void* useless ) {
 
         terrain_marque_ecrire( fd, ver[i].tete, (case_t)(i + 'A')); /* jeu_ver_initialiser le fait deja mais ne marche pas, je suis obligé de le refaire ici */
 
-        if ( verrou( fd, F_UNLCK, 0, 0, 0, getpid(), F_SETLKW) ) /* pose un verrou sur les 8 cases autours ( plus la case du milieu ) */
-            exit(-4);
-
         nbVers++;
-
-        if ( verrou( fd, F_RDLCK, 0, 0, 0, getpid(), F_SETLKW) )
-			exit(-5);
 
         if( (no_err = terrain_afficher(fd) ))
             exit(-6);
 
-        if ( verrou( fd, F_UNLCK, 0, 0, 0, getpid(), F_SETLKW) )
-            exit(-7);
     } else {
-
-        verrou9cases( fd, ver[i].tete.x, ver[i].tete.y, fdEmp, 1 );
 
         if ( ( no_err = terrain_voisins_rechercher( fd, ver[i].tete, &voisin, &nbVoisin )) )
             exit(-8);
@@ -107,80 +91,19 @@ void handler ( int sig, siginfo_t *info, void* useless ) {
             if( (no_err = terrain_marque_ecrire( fd, voisin[indLibre], (case_t)(i + 'A') ) ) )
 			    exit(-9);
 
-            verrou9cases( fd, ver[i].tete.x, ver[i].tete.y, fdEmp, 0 );
-
             ver[i].tete = voisin[indLibre];
-
-            if ( verrou( fd, F_RDLCK, 0, 0, 0, getpid(), F_SETLKW) )
-				exit(-10);
 
 			if( (no_err = terrain_afficher(fd) ))
 				exit(-11);
 
-			if ( verrou( fd, F_UNLCK, 0, 0, 0, getpid(), F_SETLKW) )
-				exit(-12);
-
         } else {
 
-            verrou9cases( fd, ver[i].tete.x, ver[i].tete.y, fdEmp, 0 );
             kill( info->si_pid, SIGUSR2 );
             nbVers--;
             if ( nbVers == 0 )
                 printf("Le vers %c a gagné !!!!! Bravo à lui !\n", i + 'A');
         }
     }
-}
-
-int verrou ( int fd, short l_type, short l_whence, long l_start, long l_len, short l_pid, int ope ) {
-
-	struct flock verrou;
-
-	verrou.l_type = l_type;
-	verrou.l_whence = l_whence;
-	verrou.l_start = l_start;
-	verrou.l_len = l_len;
-	verrou.l_pid = l_pid;
-
-	return fcntl( fd, ope, &verrou );
-}
-
-int verrou9cases( int fd, int x, int y, off_t fdEmp, int poser ) {
-
-	if ( poser ) {
-
-		terrain_xy2pos( fd, x - 1, y - 1, &fdEmp);
-
-		if ( verrou( fd, F_WRLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -1;
-
-		terrain_xy2pos( fd, x - 1, y, &fdEmp);
-
-		if ( verrou( fd, F_WRLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -2;
-
-		terrain_xy2pos( fd, x - 1, y + 1, &fdEmp);
-
-		if ( verrou( fd, F_WRLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -3;
-	} else {
-
-		terrain_xy2pos( fd, x - 1, y - 1, &fdEmp);
-
-		if ( verrou( fd, F_UNLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -4;
-
-		terrain_xy2pos( fd, x - 1, y, &fdEmp);
-
-		if ( verrou( fd, F_UNLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -5;
-
-		terrain_xy2pos( fd, x - 1, y + 1, &fdEmp);
-
-		if ( verrou( fd, F_UNLCK, 0, fdEmp, CASE_TAILLE * 3, getpid(), F_SETLKW) )
-			return -6;
-	}
-
-	return 0;
 }
 
 int main( int nb_arg , char * tab_arg[] ) {
@@ -241,14 +164,14 @@ int main( int nb_arg , char * tab_arg[] ) {
         pause();
     }
 
-    printf("alarm\n");
-
     continu = 1;
+    alarm(10);
 
     while ( continu ) {
 
         pause();
-        alarm(10);
+        alarm(0); /* arret de l'alarme */
+        alarm(10); /* remise de l'alarme pour 10 sec. */
     }
 
     printf("\n\n\t----- %s : Fin du jeu -----\n\n" , Nom_Prog );
